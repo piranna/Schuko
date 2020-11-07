cb46b1d991a49a7585208297f896d94bedb6eaf7
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2020-11-07 09:17:17 +0100

    Peers keep waiting if they have an unique peer ID

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,8 +1,9 @@
-const { createServer } = require('http');
+const { once } = require('events')
+const { createServer } = require('http')
 
 const WebSocket = require('ws')
 
-const schuko = require("..");
+const schuko = require("..")
 
 test("smoke", function () {
   expect(schuko).toMatchInlineSnapshot(`[Function]`);
@@ -14,7 +15,7 @@ test("no arguments", function () {
   expect(onUpdate).toMatchInlineSnapshot(`[Function]`);
 });
 
-test("redirect when no id", function () {
+test("redirect when there's no connection id", function () {
   const onUpdate = schuko({
     genId() {
       return "id";
@@ -38,7 +39,7 @@ test("redirect when no id", function () {
   expect(result).toMatchInlineSnapshot(`undefined`);
 });
 
-describe('id', function()
+describe('connection id', function()
 {
   let port
   let server
@@ -128,4 +129,200 @@ describe('id', function()
       })
     }, 1000)
   });
+
+  describe('peer id', function()
+  {
+    test('connect to waiting peer', function(done)
+    {
+      const ws1 = new WebSocket(`ws:localhost:${port}/room`)
+      const ws2 = new WebSocket(`ws:localhost:${port}/room?0`)
+      const ws3 = new WebSocket(`ws:localhost:${port}/room`)
+
+      const expected = 'asdf'
+
+      ws3.addEventListener('open', function()
+      {
+        ws1.close()
+
+        this.send(expected)
+      })
+
+      ws2.addEventListener('message', function({data})
+      {
+        expect(data).toBe(expected)
+
+        this.close()
+      })
+
+      ws3.addEventListener('close', function({code})
+      {
+        expect(code).toBe(1005)
+
+        done()
+      })
+    })
+
+    test("named peer keep listening", function(done)
+    {
+      const ws1 = new WebSocket(`ws:localhost:${port}/room`)
+      const ws2 = new WebSocket(`ws:localhost:${port}/room?0`)
+
+      const expected = 'asdf'
+
+      ws2.addEventListener('open', function()
+      {
+        ws1.close()
+      })
+
+      ws1.addEventListener('close', function()
+      {
+        const ws3 = new WebSocket(`ws:localhost:${port}/room`)
+
+        ws3.addEventListener('open', function()
+        {
+          ws2.send(expected)
+        })
+
+        ws3.addEventListener('message', function({data})
+        {
+          expect(data).toBe(expected)
+
+          ws2.close()
+        })
+
+        ws3.addEventListener('close', function({code})
+        {
+          expect(code).toBe(1005)
+
+          done()
+        })
+      })
+    })
+
+    describe("replace named peer with waiting one", function()
+    {
+      test("keep waiting", function()
+      {
+        const ws1 = new WebSocket(`ws:localhost:${port}/room`)
+        const ws2 = new WebSocket(`ws:localhost:${port}/room?0`)
+
+        const expected = 'asdf'
+
+        let ws3
+
+        return once(ws2, 'open')
+        .then(function()
+        {
+          ws3 = new WebSocket(`ws:localhost:${port}/room?0`)
+
+          ws1.close()
+
+          return once(ws3, 'open')
+        })
+        .then(function()
+        {
+          ws2.send(expected)
+
+          ws3.close()
+
+          return once(ws3, 'close')
+        })
+        .then(function([code])
+        {
+          expect(ws2.readyState).toBe(WebSocket.CLOSING)
+
+          expect(code).toBe(1005)
+        })
+      })
+
+      test("connect to new one", function()
+      {
+        const ws1 = new WebSocket(`ws:localhost:${port}/room`)
+        const ws2 = new WebSocket(`ws:localhost:${port}/room?0`)
+
+        const expected = 'asdf'
+
+        let ws3, ws4
+
+        return once(ws2, 'open')
+        .then(function()
+        {
+          ws3 = new WebSocket(`ws:localhost:${port}/room?0`)
+
+          ws1.close()
+
+          return once(ws3, 'open')
+        })
+        .then(function()
+        {
+          ws2.send(expected)
+
+          ws4 = new WebSocket(`ws:localhost:${port}/room`)
+
+          ws4.addEventListener('message', function({data})
+          {
+            expect(data).toBe(expected)
+
+            ws3.close()
+          })
+
+          return once(ws4, 'open')
+        })
+        .then(function()
+        {
+          expect(ws2.readyState).toBe(WebSocket.CLOSING)
+
+          return once(ws3, 'close')
+        })
+        .then(function([code])
+        {
+          expect(ws2.readyState).toBe(WebSocket.CLOSED)
+
+          expect(code).toBe(1005)
+        })
+      })
+    })
+
+    test("replace named listening peer with new one", function()
+    {
+      const ws1 = new WebSocket(`ws:localhost:${port}/room?0`)
+
+      const expected = 'asdf'
+
+      let ws2, ws3
+
+      return once(ws1, 'open')
+      .then(function()
+      {
+        ws1.send(expected)
+
+        ws2 = new WebSocket(`ws:localhost:${port}/room?0`)
+
+        return once(ws2, 'open')
+      })
+      .then(function()
+      {
+        ws3 = new WebSocket(`ws:localhost:${port}/room`)
+
+        ws3.addEventListener('message', function({data})
+        {
+          expect(data).toBe(expected)
+
+          ws2.close()
+        })
+
+        return once(ws3, 'open')
+      })
+      .then(function()
+      {
+        expect(ws1.readyState).toBe(WebSocket.CLOSED)
+
+        return once(ws3, 'close')
+      })
+      .then(function([code])
+      {
+        expect(code).toBe(1005)
+      })
+    })
+  })
 })
--- index.js
+++ index.js
@@ -12,24 +12,15 @@ function connect(socket, soc)
 {
   const {pathname, sockets} = this
 
-  // Pump and send buffered data
-  let data
-  while((data = soc.buffer.shift())) socket.send(data)
-
-  delete soc.buffer
-
-  soc.removeEventListener('message', onmessage_buffering)
-
-  // Link peers and set onmessage event to just forward between them
+  // Link peers between them
   socket.peer = soc;
   soc.peer = socket;
 
-  // Forward raw message to the other peer
-  socket.addEventListener('message', onmessage_relay)
-  soc.addEventListener('message', onmessage_relay)
-
   // Unset waiting socket (and free the connection url)
   delete sockets[pathname];
+
+  // Pump and send buffered data
+  if(!soc.is_replacing) pump(soc)
 }
 
 function onmessage_buffering({data})
@@ -42,6 +33,21 @@ function onmessage_relay({data})
 {
   this.peer.send(data);
 };
+
+function pump(soc)
+{
+  // Pump and send buffered messages
+  const {buffer, peer} = soc
+
+  let data
+  while((data = buffer.shift())) peer.send(data)
+
+  delete soc.buffer
+
+  // Forward raw messages to the other peer
+  soc.removeEventListener('message', onmessage_buffering)
+  soc.addEventListener('message', onmessage_relay)
+}
 
 function register(socket)
 {
@@ -61,45 +67,113 @@ module.exports = function({genId = nanoid, ...wsConfig} = {})
   // Dict to store connections
   const sockets = {};
 
+  // When peer connection gets closed, close the other end too
+  function onClose()
+  {
+    const {pathname, peer} = this
+
+    // Socket was not connected, remove it from sockets list
+    if(!peer)
+    {
+      delete sockets[pathname];
+      return
+    }
+
+    delete peer.peer
+
+    // Peer was annonimous, just close it
+    const {id} = peer
+    if(!id.length) return peer.close();
+
+    // There was no waiting peer, register our peer as waiting
+    const soc = sockets[pathname];
+    if(!soc)
+    {
+      peer.removeEventListener('message', onmessage_relay)
+
+      return register.call({pathname, sockets}, peer)
+    }
+
+    // Waiting named peer don't exists for this connection, connect waiting one
+    if(soc.id !== id) return connect.call({pathname, sockets}, peer, soc)
+
+    // Remove old named peer
+    peer.buffer = []
+
+    peer.removeEventListener('message', onmessage_relay)
+    peer.addEventListener('message', onmessage_buffering)
+
+    soc.is_replacing = true
+
+    // Wait until old socket gets closed, and prepend its buffered messages to
+    // the new one before start sending them
+    peer.removeEventListener('close', onClose)
+    peer.addEventListener('close', function()
+    {
+      while(this.buffer.length) soc.buffer.unshift(this.buffer.pop())
+
+      if(soc.peer) pump(soc)
+
+      soc.is_replacing = false
+    }, {once: true})
+
+    peer.close()
+  }
+
   return function(req, socket, head)
   {
     // Use `pathname` and `search` as IDs of the 'extension cord' and peer
-    let [pathname] = req.url.split('?');
+    let [pathname, ...search] = req.url.split('?');
 
     pathname = pathname.substring(1)
+    search = search.join('?')
 
     // No url to use as 'extension cord' id, generate one and redirect
     if(!pathname.length)
       return socket.end(`HTTP/1.1 302 Found\r\nLocation: /${genId()}\r\n\r\n`);
 
-    // When peer connection gets closed, close the other end too
-    function onclose()
-    {
-      // Sockets were connected, just close them
-      if(this.peer != null)
-        this.peer.close();
-
-      // Socket was not connected, remove it from sockets list
-      else
-        delete sockets[pathname];
-    }
-
     // Url with 'extension cord' id, process it
     wss.handleUpgrade(req, socket, head, function(socket)
     {
+      socket.pathname = pathname
+      socket.id = search
+
       // Look if there was a websocket waiting with this url
       const soc = sockets[pathname];
 
-      // There was a websocket waiting for this url, interconnect them
-      if(soc)
-        connect.call({pathname, sockets}, socket, soc)
-
-      // There was not a websocket waiting for this url, put it to wait itself
-      else
+      // There was not a websocket waiting for this room, put it to wait itself
+      if(!soc)
         register.call({pathname, sockets}, socket)
 
+      // There was a websocket waiting for this room with same id, replace it
+      else if(search.length && soc.id === search)
+      {
+        register.call({pathname, sockets}, socket)
+
+        // Wait until old socket gets closed, and prepend its buffered messages
+        // to the new one before start sending them
+        soc.removeEventListener('close', onClose)
+        soc.addEventListener('close', function()
+        {
+          while(this.buffer.length) socket.buffer.unshift(this.buffer.pop())
+
+          // connect.call({pathname, sockets}, this.peer, socket)
+        }, {once: true})
+
+        soc.close()
+      }
+
+      // There was a websocket waiting for this url, interconnect them
+      else
+      {
+        // Forward raw messages to the other peer
+        socket.addEventListener('message', onmessage_relay)
+
+        connect.call({pathname, sockets}, socket, soc)
+      }
+
       // When peer connection gets closed, close the other end too
-      socket.addEventListener('close', onclose)
+      socket.addEventListener('close', onClose, {once: true})
     });
   }
 }
